b2db34400fb137b6e3041140bc44eac0ee96005f
Sean <xuelun.lu@gmail.com>
2025-02-21 11:22:16 +0800

    no message

--- ckeditor_4/ckfinder_php_2.6.3/ckfinder/config.php
+++ ckeditor_4/ckfinder_php_2.6.3/ckfinder/config.php
@@ -60,7 +60,7 @@ Examples:
 
 ATTENTION: The trailing slash is required.
 */
-$baseUrl = '/ckfinder/userfiles/';
+$baseUrl = 'http://localhost/php/uploads/';
 
 /*
 $baseDir : the path to the local directory (in the server) which points to the
